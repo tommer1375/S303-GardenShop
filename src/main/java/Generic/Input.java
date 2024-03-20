133336d1253d313309552fdabfa55de29af29043
Alalilacias <andresallegue@yahoo.es>
2024-03-20 10:09:28 +0100

    Work done at the Academy on the 20/03/2024, from 10-00-14:00h.

---
+++ src/main/java/Generic/Input.java
@@ -0,0 +1,136 @@
+package Generic;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+@SuppressWarnings({"DataFlowIssue", "unused"})
+public class Input {
+    static Scanner s = new Scanner(System.in);
+    static boolean exceptionCaught;
+
+//    Methods to implement when catching InputMismatchException
+    public static byte readByte(String message){
+        byte answer = 0;
+
+        do {
+            try{
+                System.out.println(message);
+                answer = s.nextByte();
+                exceptionCaught = false;
+            } catch (InputMismatchException ignored) {
+                System.out.println("A whole number ranging from -128 to 127 is expected.");
+                exceptionCaught = true;
+            }
+        } while(exceptionCaught);
+
+        return answer;
+    }
+    public static int readInt(String message){
+        int answer = 0;
+
+        do {
+            try{
+                System.out.println(message);
+                answer = s.nextInt();
+                exceptionCaught = false;
+            } catch (InputMismatchException ignored) {
+                System.out.println("A whole number ranging from -2147483648 to 2147483647 is expected");
+                exceptionCaught = true;
+            }
+        } while(exceptionCaught);
+
+        return answer;
+    }
+    public static float readFloat(String message){
+        float answer = 0;
+
+        do {
+            try{
+                System.out.println(message);
+                answer = s.nextFloat();
+                exceptionCaught = false;
+            } catch (InputMismatchException ignored) {
+                System.out.println("A fractional number ranging from from 3.4e−038 to 3.4e+038 is expected");
+                exceptionCaught = true;
+            }
+        } while(exceptionCaught);
+
+        return answer;
+    }
+    public static double readDouble(String message){
+        double answer = 0;
+
+        do {
+            try{
+                System.out.println(message);
+                answer = s.nextDouble();
+                exceptionCaught = false;
+            } catch (InputMismatchException ignored) {
+                System.out.println("A fractional number ranging from 1.7e−308 to 1.7e+308 is expected");
+                exceptionCaught = true;
+            }
+        } while(exceptionCaught);
+
+        return answer;
+    }
+
+//    Methods to implement when catching exceptions from the exception class
+    public static char readChar(String message){
+        char answer = ' ';
+
+        do {
+            try{
+                System.out.println(message);
+                answer = s.next().charAt(0);
+                exceptionCaught = false;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                exceptionCaught = true;
+            }
+        } while(exceptionCaught);
+
+        return answer;
+    }
+    public static String readString(String message){
+        String answer = "";
+
+        do {
+            try{
+                System.out.println(message);
+                s.nextLine();
+                answer = s.nextLine();
+                exceptionCaught = false;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                exceptionCaught = true;
+            }
+        } while(exceptionCaught);
+
+        return answer;
+    }
+    public static boolean readIfNo (String message){
+        String tempAnswer;
+        boolean answer = false;
+
+        do {
+            try{
+                System.out.println(message);
+                tempAnswer = s.next();
+                exceptionCaught = false;
+
+                if(tempAnswer.equalsIgnoreCase("y") || tempAnswer.equalsIgnoreCase("true")) {
+                    answer = true;
+                } else if (tempAnswer.equalsIgnoreCase("n") || tempAnswer.equalsIgnoreCase("false")) {
+                    answer = false;
+                } else {
+                    throw new Exception();
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                exceptionCaught = true;
+            }
+        } while(exceptionCaught);
+
+        return answer;
+    }
+}
--- src/main/java/Mongo/src/Helper.java
+++ src/main/java/Mongo/src/Helper.java
@@ -1,7 +1,41 @@
 package Mongo.src;
 
+import Generic.Input;
+
 public class Helper {
     protected static void managerStart(){
+        while(true){
+            switch (Input.readInt("""
+                    Welcome to our GardenShopManager™! Choose your option:
+                    1. Create Garden Shop.
+                    2. See Currently Active Garden Shops.
+                    3. Enter a Garden Shop's internal management systems.
+                    4. Exit.
+                    """)){
+                case 1:
+                    createGardenShop();
+                    break;
+                case 2:
+                    seeActiveGardenShops();
+                    break;
+                case 3:
+                    enterGardenShop();
+                    break;
+                case 4:
+                    System.out.println("Thank you for your hard work, see you next time.");
+                    System.exit(0);
+                default:
+                    System.out.println("Unfortunately, that option isn't included in our system.");
+            }
+        }
+    }
+    private static void createGardenShop(){
+
+    }
+    private static void seeActiveGardenShops(){
+
+    }
+    private static void enterGardenShop(){
 
     }
 }
